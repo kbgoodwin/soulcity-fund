ebf2ab34e9e74bef6ab5c41bad0f67f5e7459db7
kbgoodwin <khalidgoodwin@me.com>
2025-07-08 12:22:31 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import '../styles/globals.css';
-<<<<<<< HEAD
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
@@ -10,4 +9,4 @@ import type { AppProps } from 'next/app';
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
->>>>>>> b27aaed2a53c60f6531d9c52c524beb51a00de46
+
